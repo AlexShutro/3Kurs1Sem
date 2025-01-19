b9fdb8e3178e06879b578293a4ccb64154cd31a1
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2025-01-19 19:34:45 +0300

    Add files via upload

---
+++ OC/lab8/OS08_03/OS08_03/OS08_03.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+#define KB (1024)
+#define MB (1024 * KB)
+#define PG (4 * KB)
+
+void saymem()
+{
+    MEMORYSTATUS ms;
+    GlobalMemoryStatus(&ms);
+    cout << "Объём физической памяти:      " << ms.dwTotalPhys / KB << " KB\n";
+    cout << "Доступно физической памяти:   " << ms.dwAvailPhys / KB << " KB\n";
+    cout << "Объем виртуальной памяти:     " << ms.dwTotalVirtual / KB << " KB\n";
+    cout << "Доступно виртуальной памяти:  " << ms.dwAvailVirtual / KB << " KB\n\n";
+}
+
+/*
+    Ш - 216(10) - D6(16)
+    у - 243(10) - D0(16)
+    т - 242(10) - F0(16)
+    Страница D6 = 214
+    216 * 4096 = 874240(10) = 0xD52C0 - добавить для перехода на страницу
+    Смещение D0D = 3341(10) = 0x00000C0C
+    Искомое значение: начало массива + 0xD52C0 + 0x00000C0C
+*/
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int pages = 256;
+    int countItems = pages * PG / sizeof(int);
+    SYSTEM_INFO system_info;
+    GetSystemInfo(&system_info);
+
+    cout << "\t    Изначально в системе\n";
+    saymem();
+
+    LPVOID xmemaddr = VirtualAlloc(NULL, pages * PG, MEM_COMMIT, PAGE_READWRITE);   // выделено 1024 KB виртуальной памяти
+    cout << "\tВыделено " << pages * PG / 1024 << " KB вирт. памяти\n";
+    saymem();
+
+    int* arr = (int*)xmemaddr;
+    for (int i = 0; i < countItems; i++)
+        arr[i] = i;
+
+    int* byte = arr + 216 * 1024 + 3341;
+    cout << "------  Значение памяти в байте: " << *byte << "  ------\n";
+
+    VirtualFree(xmemaddr, NULL, MEM_RELEASE) ? cout << "\tВиртуальная память освобождена\n" : cout << "\tВиртуальная память не освобождена\n";
+    saymem();
+}
