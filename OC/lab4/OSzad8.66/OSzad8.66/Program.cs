ae32cb5698bd69392b868ded751e4eda671e946c
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2025-01-19 19:29:39 +0300

    Add files via upload

---
+++ OC/lab4/OSzad8.66/OSzad8.66/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int Count = 0; // Общая переменная
+    static object lockObject = new object(); // Объект для синхронизации
+
+    static void WorkThread()
+    {
+        for (int i = 0; i < 5000000; ++i)
+        {
+            // Синхронизация доступа к переменной Count
+            lock (lockObject)
+            {
+                Count = Count + 1;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread[] threads = new Thread[20];
+
+        // Запуск 20 потоков
+        for (int i = 0; i < 20; ++i)
+        {
+            threads[i] = new Thread(WorkThread);
+            threads[i].Start();
+        }
+
+        // Ожидание завершения всех потоков
+        for (int i = 0; i < 20; ++i)
+        {
+            threads[i].Join();
+        }
+
+        Console.WriteLine($"Результат: {Count}");
+        Console.WriteLine($"Ожидаемое значение: {20 * 5000000}");
+    }
+}
