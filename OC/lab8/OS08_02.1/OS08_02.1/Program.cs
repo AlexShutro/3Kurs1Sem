dcab16ff1a9c1f8891fb2242089392e988f45017
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2025-01-19 19:34:18 +0300

    Add files via upload

---
+++ OC/lab8/OS08_02.1/OS08_02.1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        int mem = 0; // грубая оценка, нужно спросить у системы 
+        List<Big> lbig = new List<Big>(1000); 
+        while (true) 
+        { 
+            lbig.Add(new Big()); 
+            mem += 1048576 * 128; 
+            Console.WriteLine("{0,-6} MB", (mem / 1048576)); 
+            Thread.Sleep(5000); 
+        } 
+    } 
+} 
+ 
+class Big 
+{ 
+    public Int32[] IntArray; 
+    public Big() 
+    { 
+        IntArray = new int[1048576 * 32]; 
+    } 
+}
